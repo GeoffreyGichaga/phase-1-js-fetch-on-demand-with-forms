dc24659aa1012329748bb29371ed173747a6f4b9
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-24 07:19:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
+
+    inputForm.addEventListener('submit',(event)=>{
+        event.preventDefault()
+
+        const input = document.getElementById('searchByID')
+        
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res=>res.json())
+        .then(data=>{
+            const title = document.querySelector('section#movieDetails h4')
+            const paragraph = document.querySelector('section#movieDetails p')
+
+            title.textContent = data.title
+            paragraph.textContent = data.summary
+        })
+
+        
+    })
   
 }
 
